dc8a2a93f1b5029cf0612406c92947998ec8c232
heroazure <1147024669@qq.com>
2017-05-08 22:41:20 +0800

    '添加vuex'

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App'
 import router from './router'
 import { Button, Cell,Field } from 'mint-ui'
-
+Vue.use(Vuex)
 Vue.component(Button.name, Button)
 Vue.component(Cell.name, Cell)
 Vue.component(Field.name, Field)
